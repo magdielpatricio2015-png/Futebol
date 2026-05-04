57e10d825009f69ad53bb44a5592f8ecc73c5782
magdielpatricio2015-png <magdielpatricio2015@gmail.com>
2026-05-04 00:35:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import pandas as pd
 import requests
 import streamlit as st
 
-
 # ============================================================
 # ANALISADOR FUTEBOL PRO 6.0
 # ESPN + Elo + Poisson + odds auto + xG + desfalques + live
